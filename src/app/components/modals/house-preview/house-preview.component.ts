f6a340ce591dc980650fa77daba43e6eb0017499
Osmany Torres Leyva <manytostao@gmail.com>
2020-02-23 16:39:22 -0500

    Add spinner loading on iframe load

--- src/app/components/modals/house-preview/house-preview.component.ts
+++ src/app/components/modals/house-preview/house-preview.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 
-import { BsModalRef } from "ngx-bootstrap";
+import { BsModalRef, BsModalService } from "ngx-bootstrap";
 
 import { environment } from "../../../../environments/environment";
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: "app-house-preview",
@@ -12,21 +13,48 @@ import { environment } from "../../../../environments/environment";
 })
 export class HousePreviewComponent implements OnInit {
 
+  /**
+   * Url to load in iframe
+   */
   previewUrl: SafeResourceUrl;
 
+  /**
+   * House data
+   */
   house: any;
 
+  /**
+   * Component constructor
+   * @param bsModalRef NgxBootstrap modal service instance
+   * @param sanitizer Dom sanitizer
+   * @param spinnerService Spinner service instance
+   */
   constructor(private bsModalRef: BsModalRef,
-              private sanitizer: DomSanitizer) {
+              private sanitizer: DomSanitizer,
+              private spinnerService: NgxSpinnerService
+  ) {
   }
 
+  /**
+   * On component initialization
+   */
   ngOnInit() {
+    // sanitize the url to load in the iframe
     this.previewUrl = this.sanitizer.bypassSecurityTrustResourceUrl(environment.uris.preview + "/" + this.house.slug);
-    console.log(this.previewUrl);
+    this.spinnerService.show();
   }
 
+  /**
+   * On modal close
+   */
   onClose() {
     this.bsModalRef.hide();
   }
 
+  /**
+   * On iFrame finish loading
+   */
+  onIFrameLoad() {
+    this.spinnerService.hide();
+  }
 }
