bb6911cd061e2752496983ae86eb7706f3162e3c
yelena islen san juan <yelena870513@gmail.com>
2020-03-16 23:28:23 -0400

    Update assets 2
    Update assets and wording

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,5 @@ export const environment = {
     image: 'http://site.rent.cu/uploads/main/homestay'
   },
 
-  secret: "800a0e540906b2fff8f3299eba5c4e65f7442d66d9127a55f4fb436dc9aa4f64"
+  secret: '800a0e540906b2fff8f3299eba5c4e65f7442d66d9127a55f4fb436dc9aa4f64'
 };
