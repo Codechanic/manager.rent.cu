ee44c7e272051f5a423c5cdfa52e7bb103353e0c
yelena islen san juan <yelena870513@gmail.com>
2020-08-01 23:30:07 -0400

    Allow to process multiple files for uploading.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment = {
     password: 'http://site.rent.cu/es/password',
     image: 'http://site.rent.cu/uploads/main/homestay',
     api: 'http://localhost:3000',
-    upload: 'http://site.rent.cu/es/image'
+    upload: 'http://site.rent.cu/es/multiple_image'
   },
 
   secret: '800a0e540906b2fff8f3299eba5c4e65f7442d66d9127a55f4fb436dc9aa4f64'
